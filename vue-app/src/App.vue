033db6c0237e63676acd24c4515e02dc63f82097
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-11-25 18:10:08 +0100

    initial commit - cleaned app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        
+      </nav>
+    </div>
+    <main>Cleaned</main>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
