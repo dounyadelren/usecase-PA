02a851464ee529e8e67a52ebc0a4601a40147965
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-11-26 14:15:44 +0100

    update - homeview view to host card and search component and display results

--- vue-app/src/views/HomeView.vue
+++ vue-app/src/views/HomeView.vue
@@ -1,6 +1,62 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { usePropertyStore } from "@/stores/store";
+import { ref, onMounted, computed } from "vue";
+import CardList from "@/components/CardList.vue";
+import SearchBarInput from "@/components/SearchBarInput.vue";
+import NoResults from "@/components/NoResults.vue";
+
+const propertyStore = usePropertyStore();
+const propertyList = ref<any>([]);
+const searchParam = ref<string>("");
+const noResults = ref<boolean>(false);
+const nbOfResults = ref<string>("");
+
+const getPropertyList = () => {
+  propertyStore
+    .getData()
+    .then((rep: any) => {
+      propertyList.value = rep;
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
+};
+
+onMounted(() => {
+  getPropertyList();
+});
+
+// const filteredProperties = computed(() => {
+//   const query = searchParam.value.trim().toLowerCase();
+//   if(query === '') {
+//     noResults.value = false
+//     nbOfResults.value = ''
+//     return propertyList.value
+//   }
+//   const results = propertyList.value.filter((property:any) => {
+//     return (
+//       property.type.toLowerCase().includes(query) ||
+//       property.price.toString().includes(query) ||
+//       property.zipCode.toString().includes(query) ||
+//       property.dateAvailable.zip.toLowerCase().includes(query)
+//     );
+//   });
+//   if (results.length === 0) {
+//     noResults.value = true;
+//   } else {
+//     noResults.value = false;
+//     nbOfResults.value = results.length.toString();
+//   }
+//   return results
+// });
 </script>
 <template>
-  <div class="bg-blue-500">Clean</div>
+  <main class="container mx-auto">
+    <SearchBarInput />
+    <hr class="w-full" />
+    <div class="flex flex-wrap flex-row justify-center gap-3">
+      <NoResults v-if="noResults" />
+      <CardList v-else :propertyList="propertyList" />
+    </div>
+  </main>
 </template>
