9d0a78e724d1b7a403f8a37ba6ea95a0b921710a
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-11-26 14:13:02 +0100

    add - card list component to format the results into a grid

---
+++ vue-app/src/components/CardList.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { defineProps, toRefs } from "vue";
+import { Images } from "@/types/images";
+const props = defineProps<{
+  propertyList:
+    | {
+        id: number;
+        type: string;
+        price: string;
+        zipCode: string;
+        available: boolean;
+        dateAvailable: string;
+      }[]
+    | [];
+}>();
+const { propertyList } = toRefs(props);
+</script>
+<template>
+  <div
+    class="max-w-sm rounded-lg p-5"
+    v-for="value in propertyList"
+    :key="value.id"
+  >
+    <img
+      :src="Images[value.type]"
+      class="rounded-lg img-format shadow"
+      alt=""
+    />
+    <div class="py-4 flex justify-between">
+      <p class="text-sm font-bold">{{ value.type }}</p>
+      <div class="flex gap-4 items-center content-center">
+        <span class="text-xs">{{ value.price }} €</span>
+        <span class="text-xs">{{ value.zipCode }}</span>
+        <span
+          v-if="value.available === true"
+          class="flex bg-green-200 rounded text-xs font-medium text-green-500 p-1"
+          title="Property available"
+        >
+        Available
+        </span>
+        <span
+          v-else
+          class="text-xs flex gap-1 items-center content-center"
+          title="Available on"
+          ><svg
+            class="w-3 h-3 text-gray-600"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fill="currentColor"
+              d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z"
+            /></svg
+          >{{ value.dateAvailable }}</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.img-format {
+  width: 300px;
+  height: 250px;
+  object-fit: cover;
+  object-position: center;
+}
+</style>
