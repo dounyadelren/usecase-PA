521d19a15ce466f3033d2a8a6116cef0a761f037
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-11-26 14:09:36 +0100

    add - images for different types of properties

---
+++ vue-app/src/types/images.ts
@@ -0,0 +1,8 @@
+export const ImgValues = ["House", "Appartment", "Studio", "Châlet"]
+
+export const Images: Record<typeof ImgValues[any], string> = {
+  "House": "/src/assets/img/house.jpeg",
+  "Appartment": "/src/assets/img/appartment.jpg",
+  "Studio": "/src/assets/img/studio.jpg",
+  "Châlet": "/src/assets/img/chalet.jpg"
+}
