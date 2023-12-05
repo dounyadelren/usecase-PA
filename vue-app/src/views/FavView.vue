eed0dbaee2cc81b9757ffd31bc739ac5ad4d437f
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-12-05 20:52:28 +0100

    add - fav view route + view to display fav properties

---
+++ vue-app/src/views/FavView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { usePropertyStore } from "@/stores/store";
+
+const favProperties = usePropertyStore().favProperties;
+
+</script>
+<template>
+  {{ favProperties }}
+</template>
