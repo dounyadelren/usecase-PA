398e57795d7713d67aabf2bf906c91374d3332f4
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-12-05 20:54:05 +0100

    update - add a new method to store for like toggle, checks if it is already there, or not

--- vue-app/src/stores/store.ts
+++ vue-app/src/stores/store.ts
@@ -1,43 +1,58 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
 export interface Property {
-  id?: number
-  type?: string,
-  zipCode?: string,
-  price?: string,
-  available?: boolean,
-  dateAvailable?: string
+  id?: number;
+  type?: string;
+  zipCode?: string;
+  price?: string;
+  available?: boolean;
+  dateAvailable?: string;
 }
 
 export type PropertyState = {
-  property: Property
-}
+  property: Property;
+  favProperties: Property[];
+};
 
-export const usePropertyStore = defineStore('property', {
-  state: () => ({
-    property: {
-      id: undefined,
-      type: '',
-      zipCode: '',
-      price: '',
-      available: undefined,
-      dateAvailable: ''
-    }
-  } as PropertyState),
+export const usePropertyStore = defineStore("property", {
+  // state = constante
+  state: () =>
+    ({
+      property: {
+        id: undefined,
+        type: "",
+        zipCode: "",
+        price: "",
+        available: undefined,
+        dateAvailable: "",
+      },
+      favProperties: [],
+    } as PropertyState),
   actions: {
     async getData() {
       try {
-        const response = await fetch('/src/data/data.json')
-        if(response && response.status == 200) {
+        const response = await fetch("/src/data/data.json");
+        if (response && response.status == 200) {
           const data = await response.json();
-          console.log('----', data)
-          return data
+          return data;
         } else {
-          console.error('Error: ', response.status, response.statusText)
+          console.error("Error: ", response.status, response.statusText);
         }
       } catch (err) {
-        console.error('Error ', err)
+        console.error("Error ", err);
       }
-    }
-  }
-})
+    },
+    getLikedProperties() {
+      return;
+    },
+    toggleLike(property: Property) {
+      // vérifier dans le local storage si property est pas déjà présent si non ajouter si oui l'enlever
+      if (this.favProperties.includes(property)) {
+        this.favProperties.splice(this.favProperties.indexOf(property), 1);
+      } else {
+        this.favProperties.push(property);
+      }
+    },
+  },
+  persist: true,
+});
